bff45abd98b993c567bd38a15650709de2655326
seu-username <seu-email>
2024-07-25 21:30:59 -0300

    Projeto Random

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function generateNumber(){
+    
+    const min = document.querySelector('.input-min').value
+    const max = document.querySelector('.input-max').value
+
+    console.log(min, max)
+
+
+}
